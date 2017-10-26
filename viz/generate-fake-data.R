23cc73635b1432abc8bdea743a9f59ae3f36f1fc
smehta <smehta@godaddy.com>
2017-10-26 09:37:25 -0700

    [wip] add random dates

--- viz/generate-fake-data.R
+++ viz/generate-fake-data.R
@@ -8,13 +8,28 @@ messages <- fromJSON("viz/fake-messages.json")
 num.contributions <- length(people) * length(repos) * 100
 
 generate.col <- function(from) {
-  sample(from, num.contributions, replace=TRUE)
+  sample(from, num.contributions, replace = TRUE)
 }
+
+jitter.dates <- function(dates) {
+  date.count <- length(dates)
+
+  # normal distribution of minutes, with 95% percent falling in + or - 4 hours from center
+  jittered.minutes <- round(rnorm(date.count) * 120)
+  jittered.dates <- ymd_hms(as.Date(dates) +
+                              # center around 1pm (midpoint of a 9-5 schedule)
+                              hours(12) + minutes(jittered.minutes))
+
+  return(jittered.dates)
+}
+
+times = seq(as.Date('2016/01/01'), as.Date('2017/01/01'), length.out = num.contributions) %>%
+  jitter.dates()
 
 data <- tibble(
   username = generate.col(people),
   repo = generate.col(repos),
-  timestamp = 1, # make uniform distribution of days, but normal distribution of time centered at noon
   type = generate.col(c("pr", "commit")),
+  timestamp = times,
   message = generate.col(messages)
 )
