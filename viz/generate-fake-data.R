ee946e98f38a47fa3af70262cc2ba7b1bd6ae0c9
Sivan Mehta <Sivanmehta2@gmail.com>
2017-10-26 09:42:12 -0700

    Generate Fake Data (#1)
    * add fake people and repos
    
    * add script to generate fake data
    
    * add random dates

---
+++ viz/generate-fake-data.R
@@ -0,0 +1,35 @@
+library(jsonlite)
+library(lubridate)
+library(dplyr)
+
+people <- fromJSON("viz/fake-people.json")
+repos <- fromJSON("viz/fake-repos.json")
+messages <- fromJSON("viz/fake-messages.json")
+num.contributions <- length(people) * length(repos) * 100
+
+generate.col <- function(from) {
+  sample(from, num.contributions, replace = TRUE)
+}
+
+jitter.dates <- function(dates) {
+  date.count <- length(dates)
+
+  # normal distribution of minutes, with 95% percent falling in + or - 4 hours from center
+  jittered.minutes <- round(rnorm(date.count) * 120)
+  jittered.dates <- ymd_hms(as.Date(dates) +
+                              # center around 1pm (midpoint of a 9-5 schedule)
+                              hours(12) + minutes(jittered.minutes))
+
+  return(jittered.dates)
+}
+
+times = seq(as.Date('2016/01/01'), as.Date('2017/01/01'), length.out = num.contributions) %>%
+  jitter.dates()
+
+data <- tibble(
+  username = generate.col(people),
+  repo = generate.col(repos),
+  type = generate.col(c("pr", "commit")),
+  timestamp = times,
+  message = generate.col(messages)
+)
